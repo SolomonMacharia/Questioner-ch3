75ae7b2e9a5d632048cd141b9c3bf3a4bd5670c2
Gideon Gitau <giddygitau@Gideons-MacBook-Pro-2.local>
2019-01-18 07:09:02 +0300

    [WIP #163266916] Working progress

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from migrate import create_tables
-
-create_tables()
-
+from .api.v2 import version_two_bp
 
 
 def create_app():
     app = Flask(__name__)
+    create_tables()
+    app.register(version_two_bp)
     return app
--- migrate.py
+++ migrate.py
@@ -1,17 +1,16 @@
 import os
 import psycopg2
 
-url = psycopg2.connect(database='questioner_dev', password='', host='localhost', port='5432')
-import pdb; pdb.set_trace()
-print('Connected...')
+url = psycopg2.connect(database='questioner_dev',
+                       password='', host='localhost', port='5432')
+
 
 def connect_db(url):
-    print('wwwwwwww')
     conn = url
     return conn
+
+
 def create_tables():
-    print('qqq')
-    import pdb; pdb.set_trace()
     conn = connect_db(url)
     curr = conn.cursor()
     tables = create_db_tables()
@@ -21,28 +20,30 @@ def create_tables():
     conn.commit()
 
 # Create tables
+
+
 def create_db_tables():
     users_table = '''CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
+        user_id SERIAL PRIMARY KEY,
         username VARCHAR(30) NOT NULL UNIQUE,
         email VARCHAR(255) NOT NULL UNIQUE,
-        registered DATE NOT NULL DEFAULT CURRENT_DATE,
+        registered DATE NOT NULL DEFAULT NOW(),
         isAdmin BOOLEAN NOT NULL
-    )''' 
+    )'''
 
     meetups_table = '''CREATE TABLE IF NOT EXISTS meetups(
-        id SERIAL PRIMARY KEY,
-        createdOn DATE NOT NULL DEFAULT CURRENT_DATE,
+        meetups_id SERIAL PRIMARY KEY,
+        createdOn DATE NOT NULL DEFAULT NOW(),
         location VARCHAR(255),
-        images ,
+        images VARCHAR(255),
         topic VARCHAR(255),
-        happeningOn DATE NOT NULL CURRENT_DATE,
-        tags VARCHR(255)
+        happeningOn DATE NOT NULL DEFAULT NOW(),
+        tags VARCHAR(255)
     )'''
 
     questions_table = '''CREATE TABLE IF NOT EXISTS questions(
-        id SERIAL PRIMARY KEY,
-        createdOn DATE NOT NULL CURRENT_DATE,
+        question_id SERIAL PRIMARY KEY,
+        createdOn DATE NOT NULL DEFAULT NOW(),
         createdBy INT NOT NULL,
         meetup INT NOT NULL,
         title VARCHAR(255),
@@ -51,10 +52,10 @@ def create_db_tables():
     )'''
 
     rsvps_table = '''CREATE TABLE IF NOT EXISTS rsvps(
-        id SERIAL PRIMARY KEY,
-        meetup INT NOT NULL,
-        user INT NOT NULL,
-        response VARCHAR(255)
+        rsvp_id SERIAL PRIMARY KEY,
+        meetupid INT NOT NULL,
+        response VARCHAR(255),
+        userid INT NOT NULL
     )'''
 
     tables = [users_table, meetups_table, questions_table, rsvps_table]
