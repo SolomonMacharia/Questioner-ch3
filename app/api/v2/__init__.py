a4cee9a28539105772450deea200def441e21977
Solomon <solomonmacharia7@gmail.com>
2019-01-20 12:48:38 +0300

    [feature #163266186] Modified the v2  init .py file

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from .models.user_models import Users
+from flask_restful import Api
+from .views.user_views import User
 from flask import Blueprint
 
 version_two_bp = Blueprint('api_v2', __name__, url_prefix='/api/v2')
 
 api = Api(version_two_bp)
-api.add_resource(Users, '/users')
+api.add_resource(User, '/users')
