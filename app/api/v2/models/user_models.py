713b63061901f9b43330a9c86a92c78c33310a55
Solomon <solomonmacharia7@gmail.com>
2019-01-20 12:49:29 +0300

    [feature #163266186] Modified the user models

--- app/api/v2/models/user_models.py
+++ app/api/v2/models/user_models.py
@@ -1,35 +1,15 @@
-from migrate import connect_db(url)
-
-conn = connect_db(url)
-cur = conn.cursor
+from migrate import db_connection
+conn = db_connection
+cur = conn.cursor()
 
 
 class Users:
     def __init__(self):
-        self.db = connect_db(url)
+        self.db = db_connection
 
     def create_user(self, username, email, password, confirm_password):
-        query = '''INSERT INTO users(username, email, password, confirnPassword)\
-          VALUES(%(username)s, %(email)s, %(passord)s, %(confirmPassord)s)'''
-        cur.execute(query,)
+        cur.execute('''INSERT INTO users(username, email, password, confirm_password) VALUES(%s, %s, %s, %s );''',\
+         (username, email, password, confirm_password))
         conn.commit()
 
-    def get_all(self):
-        cur.execute('''SELECT * FROM users''')
-        data = cur.fetchall()
-
-    def get_user(self, user_id):
-        query = '''SELECT * FROM users WHERE user_id = user_id'''
-        cur.execute(query,)
-        data = cur.fetchone()
-
-    def update_user(self, user_id):
-        query = '''UPDATE users SET({},{},{},{}) WHERE user_id = user_id'''.format(username,
-                                                                                   email, password, confirm_password)
-        cur.execute(query,)
-
-    def delete_user(self, user_id):
-        cur.execute(
-            '''DELETE FROM users WHERE user_id = %(user_id)s''', (user_id,))
-        conn.commit()
-        cur.close()
+    
