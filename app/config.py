ccb933d6cdc1391cb03f7612b5b25c5f6e1fcfbd
Solomon <solomonmacharia7@gmail.com>
2019-01-21 20:09:38 +0300

    [Feature #163266576] Modified config.py file

--- app/config.py
+++ app/config.py
@@ -1,29 +1,31 @@
 import os
+import psycopg2
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    ENV = 'production'
     DEBUG = False
     TESTING = False
     CSRF_ENABLED =  True
-    SECRET_KEY =  'something confidential..'
+    SECRET_KEY = b'gn%\x92\x08Zp\xffZgg~\xa4\xfcY\x8b\xb4H\xebL\xd7\xa4\x80V'
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
 class ProductionConfig(Config):
     DEBUG = False
 
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
 class DevelopmentConfig(Config):
-    DEVELOPMENT = True
+    ENV = 'development'
     DEBUG = True
+    DATABASE_URL = os.getenv('DEV_DATABASE_URL')
 
 class TestingConfig(Config):
+    ENV = 'testing'
     TESTING = True
+    DEBUG = True
+    DATABASE_URL = os.getenv('TEST_DATABASE_URL')
     
 APP_SETTINGS = {
     'production': ProductionConfig,
-    'staging': StagingConfig,
     'development': DevelopmentConfig,
     'testing': TestingConfig
 }
