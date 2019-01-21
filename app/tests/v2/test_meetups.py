36f080ba34c2ce24f26f3f967681af1c1340ef88
Solomon <solomonmacharia7@gmail.com>
2019-01-21 20:13:01 +0300

    [Chore #163266576] write the create meetup endpoint tests

---
+++ app/tests/v2/test_meetups.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+from app import create_app
+from migrate import create_tables, drop_tables
+
+class Testmeetups(unittest.TestCase):
+    def setUp(self):
+        # import pdb; pdb.set_trace()
+        self.app = create_app(environment='testing')
+        self.client = self.app.test_client()
+        self.app.testing = True
+    
+        self.meetup = {
+            "happening_on": "The day",
+            "images": "images",
+            "location": "The venue",
+            "topic": "The title "
+        }
+
+        
+    def test_meetup_registration(self):
+        resp = self.client.post('/api/v2/meetups', data=json.dumps(self.meetup), content_type='application/json')
+        self.assertEqual(resp.status_code, 201)
+
+ 
+    # def tearDown(self):
+    #     print('Dropping Tables')
+    #     drop_tables()
+    #     # pass       
+
+
+if __name__ == '__main__':
+    unittest.main()
