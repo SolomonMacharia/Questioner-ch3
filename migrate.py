4f89d7ee8d9dc405c12ee3015d1bed01fc01a6a2
Solomon <solomonmacharia7@gmail.com>
2019-01-22 12:14:35 +0300

    [feature #163375819] Modified the migrate file, Changed datetime to varchar

--- migrate.py
+++ migrate.py
@@ -46,43 +46,44 @@ def create_db_tables():
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
         confirm_password VARCHAR(255) NOT NULL,
-        registered TIMESTAMP,
+        registered VARCHAR(50),
         isAdmin BOOLEAN
-    )'''
+    );'''
     print("....users_table created")
 
     meetups_table = '''CREATE TABLE IF NOT EXISTS meetups(
         id SERIAL PRIMARY KEY,
-        created_on TIMESTAMP,
+        created_on VARCHAR(50),
         location VARCHAR(255),
         images VARCHAR(255),
         topic VARCHAR(255),
         happening_on VARCHAR(25),
         tags VARCHAR(255)
-    )'''
+    );'''
     print("....meetups_table created ")
 
     questions_table = '''CREATE TABLE IF NOT EXISTS questions(
         id SERIAL PRIMARY KEY,
         meetupid INT REFERENCES meetups(id) ON DELETE CASCADE,
-        created_on TIMESTAMP,
+        created_on VARCHAR(50),
         created_by INT NOT NULL,
         meetup INT NOT NULL,
         title VARCHAR(255),
         body VARCHAR(255),
         votes INT
 
-    )'''
+    );'''
     print("....questons_table created")
 
     rsvps_table = '''CREATE TABLE IF NOT EXISTS rsvps(
         id SERIAL PRIMARY KEY,
         meetupid INT NOT NULL,
         response VARCHAR(255)
-    )'''
+    );'''
     print("....rsvp_tables created")
 
     tables = [users_table, meetups_table, questions_table, rsvps_table]
     return tables
 
 create_tables()
+drop_tables()
