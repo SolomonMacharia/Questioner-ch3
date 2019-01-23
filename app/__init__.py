9b0d98347955a914dddb1e7168fbe3f5e3ee204d
Solomon <solomonmacharia7@gmail.com>
2019-01-23 20:30:51 +0300

    [feature #163266381] Modified the app/ init .py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ import os
 from flask import Flask
 from .api.v2 import version_two_bp
 from app.config import APP_SETTINGS
-
+from migrate import create_tables
 
 def create_app(environment=''):
     app = Flask(__name__)
+    create_tables()
     app.config.from_object(APP_SETTINGS[environment])
     app.register_blueprint(version_two_bp)
     return app
