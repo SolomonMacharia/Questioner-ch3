d24fed13590c483cff960b8a091d827f48967fb7
Solomon <solomonmacharia7@gmail.com>
2019-01-23 20:48:11 +0300

    [Chore #163266381] Test the get all questions endpoint.

---
+++ app/tests/v2/test_questions.py
@@ -0,0 +1,30 @@
+import unittest
+import json
+from app import create_app
+from migrate import create_tables, drop_tables
+
+class TestUsers(unittest.TestCase):
+    def setUp(self):
+        # import pdb; pdb.set_trace()
+        self.app = create_app(environment='testing')
+        self.client = self.app.test_client()
+        self.app.testing = True
+    
+        self.question = {
+            'title': 'title',
+            'body': 'body'
+        }
+
+        
+    def test_get_all_questions(self):
+        resp = self.client.get('/api/v2/questions', data=json.dumps(self.question), content_type='application/json')
+        self.assertEqual(resp.status_code, 200)
+
+ 
+    def tearDown(self):
+        print('Dropping Tables')
+        drop_tables()
+        # pass
+
+if __name__ == '__main__':
+    unittest.main()
