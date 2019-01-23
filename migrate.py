77c939756041d050cd17ac5f9db2f4509aa94871
Solomon <solomonmacharia7@gmail.com>
2019-01-23 03:12:20 +0300

    [Bug #163409068] Modify the migrate.py file

--- migrate.py
+++ migrate.py
@@ -2,27 +2,11 @@ import os
 import re
 import psycopg2
 
-environment = os.getenv('APP_ENV')
-database_url = None
-
-if environment == 'production':
-    database_url = os.getenv('DATABASE_URL')
-
-if environment == 'development':
-    database_url = os.getenv('DEV_DATABASE_URL')
-
-if environment == 'testing':
-    database_url = os.getenv('TEST_DATABASE_URL')
-
-
-def db_connection(database_url):
-    host, port, db_name = re.match('postgres://(.*?):(.*?)/(.*)', database_url).groups()
-    conn = psycopg2.connect("dbname={}".format(db_name))
-    return conn
+db_url = psycopg2.connect("dbname='questioner_dev'")
 
 def create_tables():
     print('connecting to db')
-    conn = db_connection(database_url)
+    conn = db_url
     cur = conn.cursor()
     tables = create_db_tables()
 
@@ -32,12 +16,10 @@ def create_tables():
 
 def drop_tables():
     '''Deletes all existing tables'''
-    conn = db_connection(database_url)
+    conn = db_url
     cur = conn.cursor()
     cur.execute('''DROP TABLE IF EXISTS users_table, meetups_table, questions_table, rsvps_table''')
     conn.commit()
-# Create tables
-
 
 def create_db_tables():
     users_table = '''CREATE TABLE IF NOT EXISTS users(
@@ -49,7 +31,6 @@ def create_db_tables():
         registered VARCHAR(50),
         isAdmin BOOLEAN
     );'''
-    print("....users_table created")
 
     meetups_table = '''CREATE TABLE IF NOT EXISTS meetups(
         id SERIAL PRIMARY KEY,
@@ -60,7 +41,6 @@ def create_db_tables():
         happening_on VARCHAR(25),
         tags VARCHAR(255)
     );'''
-    print("....meetups_table created ")
 
     questions_table = '''CREATE TABLE IF NOT EXISTS questions(
         id SERIAL PRIMARY KEY,
@@ -73,17 +53,12 @@ def create_db_tables():
         votes INT
 
     );'''
-    print("....questons_table created")
 
     rsvps_table = '''CREATE TABLE IF NOT EXISTS rsvps(
         id SERIAL PRIMARY KEY,
         meetupid INT NOT NULL,
         response VARCHAR(255)
     );'''
-    print("....rsvp_tables created")
 
     tables = [users_table, meetups_table, questions_table, rsvps_table]
     return tables
-
-create_tables()
-drop_tables()
