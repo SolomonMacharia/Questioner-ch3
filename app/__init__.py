4a9711d640546e65cc1ff0e3ee37600c17616af2
Solomon <solomonmacharia7@gmail.com>
2019-01-23 03:02:41 +0300

    [Bug #163409068] Modify the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
 from .api.v2 import version_two_bp
-from app.config import APP_SETTINGS
+from instance.config import app_settings
+from migrate import create_tables
 
-
-def create_app(environment=''):
-    app = Flask(__name__)
-    app.config.from_object(APP_SETTINGS[environment])
+def create_app(config):
+    app = Flask(__name__, instance_relative_config=True)
+    create_tables()
+    app.config.from_object(app_settings[config])
     app.register_blueprint(version_two_bp)
     return app
