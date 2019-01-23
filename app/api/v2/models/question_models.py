30427294dd94b4b85f718144f813b05ddddb63ef
Solomon <solomonmacharia7@gmail.com>
2019-01-23 20:33:19 +0300

    [feature #163266381] Create the get all questions method in the database

--- app/api/v2/models/question_models.py
+++ app/api/v2/models/question_models.py
@@ -1,4 +1,12 @@
 from . import conn, cur
+from psycopg2.extras import RealDictCursor
+
 
 class QuestionModels(object):
-    pass
+    """Fetches all questions from the db"""
+    def fetch_all_questions(self):
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute("SELECT id, created_on, created_by, title, body, votes FROM questions ORDER BY votes")
+            rows = cur.fetchall()
+            return rows
+
