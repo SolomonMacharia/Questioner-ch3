76df86facdbc990976436ec7fcde8efb76fa530a
Solomon <solomonmacharia7@gmail.com>
2019-01-24 11:34:34 +0300

    [finishes #163445836] Create post question endpoint

--- app/api/v2/views/question_views.py
+++ app/api/v2/views/question_views.py
@@ -9,7 +9,19 @@ import re
 questions_db = QuestionModels()
 
 class Questions(Resource):
-    '''This class handles the meetup endpoints'''
+    '''This class handles the questions endpoints'''
+    def post(self):
+        # import pdb; pdb.set_trace()
+        user_data = request.get_json()
+        data = validate(user_data, required_fields=['title', 'body', 'votes'])
+        if type(data) == list:
+            return {"status": 400, "errors": data}, 400
+        title = data["title"]
+        body = data['body']
+        votes = data['votes']
+
+        questions_db.create_question( title, body, votes)
+        return {"status": 201, "Message": "Question ' {} ' created!". format(title)}, 201
     def get(self):
         all_questions = questions_db.fetch_all_questions()
         return {"status": 200, "data": all_questions}, 200
