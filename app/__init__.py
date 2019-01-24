d2e3178d105834f849e12a6992aea1bfeb93fe54
Solomon <solomonmacharia7@gmail.com>
2019-01-24 06:21:19 +0300

    [feature 163431812] Modified init .py

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask import Flask
 from .api.v2 import version_two_bp
 from app.config import APP_SETTINGS
 from migrate import create_tables
+from flask_jwt_extended import JWTManager
 
 def create_app(environment=''):
     app = Flask(__name__)
     create_tables()
     app.config.from_object(APP_SETTINGS[environment])
+    jwt = JWTManager(app)
     app.register_blueprint(version_two_bp)
     return app
