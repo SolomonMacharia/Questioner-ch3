9db904b4f1f52a8a490194a29cbde8090019a34c
Solomon <solomonmacharia7@gmail.com>
2019-01-24 06:24:21 +0300

    [feature 163431812] Modified user views.py

--- app/api/v2/views/user_views.py
+++ app/api/v2/views/user_views.py
@@ -3,6 +3,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_restful import Resource, reqparse
 from ..utils.validator import validate
 from ..models.user_models import Users
+from datetime import datetime
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, create_refresh_token
 import re
 
 user_db = Users() 
@@ -31,8 +33,30 @@ class User(Resource):
         return {"status": 201, "Message": "User {} created!". format(username)}, 201
 
     def get(self):
-        pass
-
+        users = user_db.get_all_user()
+        return {'status':200, 'data': users}
+    
 
 class SingleUser(User):
-    pass
+    def get(self, id):
+        single_user = user_db.get_single_user(id)
+        return {"status": 200, "data": single_user}
+
+class UserLogin(Resource):
+    def post(self):
+        user_data = request.get_json()
+        username = user_data["username"]
+        password = user_data["password"]
+        current_user = user_db.get_user_by_username(username)
+        data = validate(user_data, required_fields=['username', 'password', 'confirm_password'])
+        if type(data) == list:
+            return {"status": 400, "errors": data}, 400
+        if not current_user:
+            abort(404, "User {} doesn't exist!".format(username))
+        if  check_password_hash(current_user['password'], user_data['password']):
+            access_token = create_access_token(identity=user_data['username'])
+            return {'message': 'login success', 'access_token': access_token}, 200
+        abort(401, "Invalid credentials!")
+        return None
+            
+
