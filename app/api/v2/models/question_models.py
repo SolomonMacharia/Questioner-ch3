dfd1d3ae5ad46c790646b480f6f3c16dafd539fb
Solomon <solomonmacharia7@gmail.com>
2019-01-24 11:33:16 +0300

    [feature #163445836] modified question models file

--- app/api/v2/models/question_models.py
+++ app/api/v2/models/question_models.py
@@ -1,12 +1,21 @@
 from . import conn, cur
 from psycopg2.extras import RealDictCursor
+from datetime import datetime
 
 
 class QuestionModels(object):
-    """Fetches all questions from the db"""
+    def __init__(self):
+        self.created_on = datetime.now()
+
+    def create_question(self, title, body, votes):
+        cur.execute('''INSERT INTO questions(created_on, title, body, votes) VALUES(%s, %s, %s, %s );''',\
+                    (self.created_on, title, body, votes))
+        conn.commit()
+    
     def fetch_all_questions(self):
+        """Fetches all questions from the db"""
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT id, created_on, created_by, title, body, votes FROM questions ORDER BY votes")
+            cur.execute("SELECT id, created_on, title, body, votes FROM questions ORDER BY votes")
             rows = cur.fetchall()
             return rows
 
