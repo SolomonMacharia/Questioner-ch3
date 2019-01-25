ab9101b16407c1b8354c8129ea89346ef7461ca7
Solomon <solomonmacharia7@gmail.com>
2019-01-25 07:43:29 +0300

    [feature #163266504] Modified user models file

--- app/api/v2/models/user_models.py
+++ app/api/v2/models/user_models.py
@@ -5,30 +5,40 @@ from functools import wraps
 from app import config
 import jwt
 
+
 class Users:
     def __init__(self):
         pass
 
     def create_user(self, username, email, password, confirm_password):
-        cur.execute('''INSERT INTO users(username, email, password, confirm_password) VALUES(%s, %s, %s, %s );''',\
-         (username, email, password, confirm_password))
-        conn.commit()
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute('''INSERT INTO users(username, email, password, confirm_password) VALUES(%s, %s, %s, %s ) RETURNING username, email;''',
+                        (username, email, password, confirm_password))
+            created_user = cur.fetchone()
+            conn.commit()
+            cur.execute('ROLLBACK')
+
+            return created_user
 
     def get_all_user(self):
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT id, username, email FROM users ORDER BY id" )
+            cur.execute("SELECT id, username, email FROM users ORDER BY id")
             rows = cur.fetchall()
             return rows
 
     def get_single_user(self, id):
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT username, email FROM users WHERE id = {}".format(id) )
-            rows = cur.fetchone()
-            return rows
-    
-    def get_user_by_username(self, username):
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT * FROM users WHERE username = %s;", (username,))
+            cur.execute(
+                "SELECT username, email FROM users WHERE id = {}".format(id))
             rows = cur.fetchone()
             return rows
 
+    def get_user_by_email(self, email):
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+
+            email_parameter = "\'{}\'".format(email)
+            query = 'SELECT * from users where email = {}'.format(
+                email_parameter)
+            cur.execute(query)
+            rows = cur.fetchone()
+            return rows
