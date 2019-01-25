8e489ea87f9b083c0f3176039cfdb8909aa7bbda
Solomon <solomonmacharia7@gmail.com>
2019-01-25 07:49:26 +0300

    [feature #163266504] Modified run.py  file

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-import os
-# import re
-# import psycopg2
-
 from app import create_app
+import os
+from flask_jwt_extended import JWTManager
 
 environment = os.getenv('APP_ENV')
 
 app = create_app(environment)
+
+jwt = JWTManager(app)
