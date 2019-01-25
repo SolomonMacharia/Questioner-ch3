ea352b5fd4b80ea1202ad155c170cae02b51c83d
Solomon <solomonmacharia7@gmail.com>
2019-01-25 07:41:36 +0300

    [feature #163266504] Modified meetup models file

--- app/api/v2/models/meetup_models.py
+++ app/api/v2/models/meetup_models.py
@@ -3,35 +3,44 @@ from flask import jsonify, abort
 from datetime import datetime
 from psycopg2.extras import RealDictCursor
 
+
 class Meetups:
-    ''''This is the meetups model that aids in DB transactions''' 
+    ''''This is the meetups model that aids in DB transactions'''
+
     def __init__(self):
         self.created_on = datetime.now()
         pass
 
-    def create_meetup(self,location, images, topic, happening_on):
+    def create_meetup(self, location, images, topic, happening_on, current_user):
         '''Adds a new meetup into the database'''
-        cur.execute('''INSERT INTO meetups(created_on, location, images, topic, happening_on) VALUES(%s, %s, %s, %s, %s );''',\
-                    (self.created_on, location, images, topic, happening_on))
-        conn.commit()
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute('''INSERT INTO meetups(created_on, location, images, topic, happening_on, created_by) VALUES(%s, %s, %s, %s, %s, %s) RETURNING *;''',
+                        (self.created_on, location, images, topic, happening_on, current_user))
+            created_meetup = cur.fetchone()
+            conn.commit()
+            return created_meetup
+
     def get_all_meetups(self):
         """Fetches all meetups from the database"""
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT id, created_on, location, images, topic, happening_on FROM meetups ORDER BY id")
+            cur.execute(
+                "SELECT id, created_on, location, images, topic, happening_on FROM meetups ORDER BY id")
             rows = cur.fetchall()
             return rows
+
     def get_one_meetup(self, id):
         """Fetches a single meetup from the db based on it's id"""
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("SELECT id, created_on, location, images, topic, happening_on, tags FROM meetups WHERE id = {}".format(id) )
-            # if id == None:
-            #     abort(400, "Question {} doesn't exist!".format(id) )
+            cur.execute(
+                "SELECT id, created_on, location, images, topic, happening_on FROM meetups WHERE id = {}".format(id))
             rows = cur.fetchone()
             return rows
 
     def delete_meetup(self, id):
         """This methods deletes a meetup from the db based on the its id number."""
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute("DELETE FROM meetups WHERE id = {}".format(id))
+            cur.execute(
+                "DELETE FROM meetups WHERE id = {} RETURNING *".format(id))
+            deleted = cur.fetchone()
             conn.commit()
-            return {"status": 200, "Message": "Meetup deleted"}
+            return deleted
